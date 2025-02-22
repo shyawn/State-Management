e7a742a202eb24265d688a4550bbfc3b082142ed
ShawnCsj <csjshawn@gmail.com>
2025-02-22 14:24:13 +0800

    feat: add navigation wrapper

--- App.tsx
+++ App.tsx
@@ -1,60 +1,21 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  Button,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import {StatusBar, StyleSheet, useColorScheme} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Home from './src/pages/Home';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+const Stack = createNativeStackNavigator();
 
 function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
 
   return (
-    <View style={styles.homeContainer}>
-      <View style={styles.headerContainer}>
-        <Text style={styles.homeHeader}>Welcome to DailyVita</Text>
-        <Text style={styles.homeDescription}>
-          Hello, we are here to make your life healthier and happier
-        </Text>
-      </View>
-      <Button title="Get Started"></Button>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  homeContainer: {
-    padding: 20,
-    paddingVertical: 30,
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  headerContainer: {
-    flex: 1,
-  },
-  homeHeader: {
-    fontSize: 24,
-    fontWeight: 600,
-  },
-  homeDescription: {
-    fontSize: 13,
-  },
-});
 
 export default App;

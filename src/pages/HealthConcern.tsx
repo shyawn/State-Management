2d4504ba734cb9987c15e8c1a9fdd0fcba0749df
ShawnCsj <csjshawn@gmail.com>
2025-02-22 15:30:36 +0800

    feat: create reducer to toggle health concern option

--- src/pages/HealthConcern.tsx
+++ src/pages/HealthConcern.tsx
@@ -2,20 +2,28 @@ import {View, Text} from 'react-native';
 import React from 'react';
 import {useNavigation} from '@react-navigation/native';
 import Button from '../components/Button';
+import {increment} from '../store/healthConcern/healthSlice';
+import {AppDispatch, RootState} from '../store/store';
+import {useDispatch, useSelector} from 'react-redux';
 
 export default function HealthConcern() {
   const navigation = useNavigation();
+  const count = useSelector((state: RootState) => state.healthConcern);
+  const dispatch = useDispatch<AppDispatch>();
+
   const handleBack = () => {
     return navigation.goBack();
   };
   const handlePress = () => {
-    return navigation.navigate('Diets');
+    // return navigation.navigate('Diet');
+    return dispatch(increment());
   }; // todo: refactor for multiple cases
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>Select the top health concerns.* (up to 5)</Text>
       <Button title="Go Back" onPress={handleBack} />
       <Button title="Next" onPress={handlePress} />
+      <Text>{count}</Text>
     </View>
   );
 }
--- src/store/healthConcern/healthSlice.ts
+++ src/store/healthConcern/healthSlice.ts
@@ -1,18 +1,43 @@
-import {createSlice} from '@reduxjs/toolkit';
+import {createSlice, PayloadAction} from '@reduxjs/toolkit';
 
-interface HealthConcern {
-  id: string;
-  name: string;
-  priority: number;
-}
+// interface HealthConcern {
+//   id: string;
+//   name: string;
+//   priority: number;
+// }
 
-const initialState: HealthConcern[] = [];
+// const initialState: HealthConcern[] = [];
+const initialState: string[] = [
+  'Sleep',
+  'Immunity',
+  'Stress',
+  'Joint Support',
+  'Digestion',
+  'Mood',
+  'Energy',
+  'Hair, Skin, Nails',
+  'Weight Loss',
+  'Fitness',
+];
 
 const healthSlice = createSlice({
   name: 'healthConcern',
   initialState,
-  reducers: {},
+  reducers: {
+    // Toggles concern option based on payload received
+    chooseOption: (state, action: PayloadAction<string>) => {
+      const index = state.indexOf(action.payload);
+      if (index === -1) {
+        state.push(action.payload);
+      } else {
+        state.splice(index, 1);
+      }
+    },
+    // setConcernOrder: (state, action) => {
+    //   return action.payload;
+    // },
+  },
 });
 
-export const {} = healthSlice.actions;
+export const {chooseOption} = healthSlice.actions;
 export default healthSlice.reducer;
